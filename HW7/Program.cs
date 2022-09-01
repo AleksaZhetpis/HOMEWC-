6e05d9a102f52da4a763b1a1ad53bee9a86d2475
«Aleksandra <smusokhranova@mail.ru>
2022-09-01 23:32:28 +0700

    HW7

---
+++ HW7/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+/*Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+Array1(array);
+
+Array2(array);
+
+Console.WriteLine();
+
+void Array1(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void Array2 (double[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double Number = Math.Round(array[i, j], 1);
+        Console.Write(Number + " ");
+      }
+      Console.WriteLine();
+  }
+}
+*/
+
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+
+//1 4 7 2
+
+//5 9 2 3
+
+//8 4 2 4
+
+//17 -> такого числа в массиве нет
+
+
+/*Console.WriteLine("Введите размеры массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("Числа не существует.");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
+
+*/
+
+
