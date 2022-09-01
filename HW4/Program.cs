e79684c43fc08ee92334ac7afd24a90cbb40b77c
«Aleksandra <smusokhranova@mail.ru>
2022-09-01 23:18:27 +0700

    HW4

---
+++ HW4/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+int summa = 0;
+
+while (i > 0)
+{
+int number = i % 10;
+i = i / 10;
+summa = summa + number;
+}
+Console.WriteLine("Сумма цифр в числе: " + summa);
+*/
+
+//Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+/*
+int [] numbers = new int[9]; // для примера 9 чисел в массиве
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 10); 
+    Console.Write(" " + Numbers (i) + " ");
+ }
+Console.Write("]");
+
+int Numbers (int num)
+{
+    return numbers[num];
+}
+*/
